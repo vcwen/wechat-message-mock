f2be08605118c8906a448d812aac42e697f345db
vincent.wen <wenwei1202@gmail.com>
2017-03-30 15:32:01 +0800

    helper class

---
+++ src/MessageHelper.js
@@ -0,0 +1,37 @@
+const crypto = require('crypto')
+const ejs = require('ejs')
+
+const encryptedTpl = '<xml><Encrypt><![CDATA[<%-encrypt%>]]></Encrypt></xml>'
+const compiledTpl = ejs.compile(encryptedTpl)
+
+class MessageHelper {
+  static generateSignature(token, nonce, timestamp) {
+    const shasum = crypto.createHash('sha1')
+    const arr = [token, timestamp, nonce].sort()
+    shasum.update(arr.join(''))
+    const signature = shasum.digest('hex')
+    return signature
+  }
+
+  static generateNonce() {
+    const regex = /[1-9]\d{9}/
+    const match = regex.exec(Math.random().toString())
+    if (match) {
+      return match[0]
+    } else {
+      throw new Error('Failed to generate nonce string')
+    }
+  }
+  static getTimestamp() {
+    return Number.parseInt(Date.now() / 1000)
+  }
+  static encryptMessage(wechatEncryptor, content) {
+    const encryptMessage = wechatEncryptor.encrypt(content)
+    const encryptXml = compiledTpl({
+      encrypt: encryptMessage,
+    })
+    return encryptXml
+  }
+}
+
+module.exports = MessageHelper
