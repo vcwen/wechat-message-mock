95a59e0971ca005c31b5f246c9b8bbaa017cb7b9
vincent.wen <wenwei1202@gmail.com>
2017-07-05 14:49:31 +0800

    export MessageHelper

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const WechatMessage = require('./src/WechatMessage')
 const Sender = require('./src/Sender')
+const MessgeHelper = require('./src/MessageHelper')
 
 module.exports = {
   WechatMessage,
   Sender,
+  MessgeHelper,
 }
