33e83053a690a79a47a9dd23de043098c7032f8a
Vincent Wen <wenwei1202@gmail.com>
2017-07-10 23:18:31 +0800

    remove js config

--- main.js
+++
@@ -1,9 +0,0 @@
-const WechatMessage = require('./src/WechatMessage')
-const Sender = require('./src/Sender')
-const MessageHelper = require('./src/MessageHelper')
-
-module.exports = {
-  WechatMessage,
-  Sender,
-  MessageHelper,
-}
